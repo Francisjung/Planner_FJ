5b782367620e8e1c5346d96663091dd911b9dd4e
Francis Jung <francisjung17@gmail.com>
2023-04-06 01:54:29 -0500

    added js changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,28 @@
+/*
+Author: Francis Jung
+Date:   4/6/2023
+*/
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+//Initializes each block by checking the relative time and to see if there is any saved data in local storage.
 $(function () {
   for(var i=0; i<9; i++){
     var slot = document.getElementById("parent-container").children[i];
     slot.classList.add(isPPF(slot.id));
+    hasSavedData("hour-"+i);
   }
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
- 
-  //Event listener for saveBtn
-  var saveBtn = $('.saveBtn');
-  saveBtn.on("click", function() {
+
+  //Event listener for saveBtn. On click it will save the id and text content of the desired section.
+  $('.saveBtn').on("click", function() {
     var parentID = this.parentNode.id;
     var timeDesc = this.parentNode.children[1].value;
-    console.log(parentID)
-    console.log(timeDesc)
-    //REVISIT THIS, IT DOES NOT WORK AS INTENDED
-    localStorage.setItem(parentID, JSON.stringify())
-    localStorage.setItem(timeDesc, JSON.stringify())
+    localStorage.setItem(parentID, timeDesc)
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
+  //Checks the time of the desired id and compares it to the current time, then returns whether it is in the past, present, or future.
   function isPPF(time){
     timeArray = time.split('-');
     timeNum= timeArray[1];
@@ -50,10 +41,15 @@ $(function () {
       return;
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
+  //Checks local storage to see if there is a saved string for the desired hour, if there is it will apply that string to the text area of the page.
+  function hasSavedData(hour){
+    if(localStorage.getItem(hour)!=null){
+      getElementById(hour).children[1].textContent= localStorage.getItem(hour);
+    }
+  }
+
+  //Prints the data at the top of the page
   var today = dayjs();
   $('#currentDay').text(today.format('MM-DD-YYYY'));
 });
